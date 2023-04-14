0764e110fadbb147c877fbc650013627b8da2165
Stefanos <stefanos26sophocleous@gmail.com>
2023-04-14 14:41:41 -0700

    Project Completed. CRUD WORKS -> SQLITE3 implemented

--- Milestone2/utils/Database_connection.py
+++ Milestone2/utils/Database_connection.py
@@ -2,10 +2,16 @@ import sqlite3
 # C
 class DataBaseConnection:
     
-    def __init__(self):
+    def __init__(self, host):
         self.connection = None
+        self.host = host
     def __enter__(self):
-        self.connection =  sqlite3.connect('data.db')
+        self.connection =  sqlite3.connect(self.host)
         return self.connection
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+    def __exit__(self, exc_type, exc_val, exc_tb): # Database could be left in an inconsistant state. 
+        if exc_type or exc_val or exc_tb:
+            self.connection.close()
+        else:
+            self.connection.commit()
+            self.connection.close()
+
--- Milestone2/utils/database.py
+++ Milestone2/utils/database.py
@@ -4,45 +4,31 @@ books_file = 'books.txt'
 
 
 def create_book_table():
-    with DataBaseConnection() as connection: ## { Connection is whatever the DUNDER __Enter__ method returned}
+    with DataBaseConnection('data.db') as connection: ## { Connection is whatever the DUNDER __Enter__ method returned}
         cursor = connection.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)') #Table
 
-    #connection.commit() # Push the DB.
-   # connection.close() # Close the connection
-
-
 def get_all_books():
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM books') # Execute this
-
+      with DataBaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM books') # Execute this
 # This allows us to end iwth a dictionary
-    books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()] ## you wil get a list of tuples. [(name, author, read), (name, author, read)]
-    
-
-    connection.close()
-    return books
+        books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()] ## you wil get a list of tuples. [(name, author, read), (name, author, read)]
+        return books
+      
 def add_book(name, author):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO books VALUES (?, ?, 0)', (name, author)) #This is saafer
-
-    connection.commit() # Push the DB.
-    connection.close() # Close the conn
+      with DataBaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO books VALUES (?, ?, 0)', (name, author)) #This is saafer
 
 def mark_book_as_read(name):
     ## mark all books as read
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-    cursor.execute('UPDATE books SET read=1 WHERE name=?', (name)) ## Update the book where the name is equal
-    connection.commit()
-    connection.close()
+        with DataBaseConnection('data.db') as connection:
+            cursor = connection.cursor()
+        cursor.execute('UPDATE books SET read=1 WHERE name=?', (name)) ## Update the book where the name is equal
 
 def delete_book(name):
     ## mark all books as read
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-    cursor.execute('DELETE FROM books WHERE name=?', (name)) ## Update the book where the name is equal
-    connection.commit()
-    connection.close()
+        with DataBaseConnection('data.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute('DELETE FROM books WHERE name=?', (name)) ## Update the book where the name is equal
