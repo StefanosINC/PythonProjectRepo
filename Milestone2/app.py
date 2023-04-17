ed43e9b6366465aab05e2a8df3f7c2bbf54bde13
Stefanos <stefanos26sophocleous@gmail.com>
2023-04-17 07:08:43 -0700

    push

--- Milestone2/app.py
+++ Milestone2/app.py
@@ -30,7 +30,7 @@ def menu():
 def prompt_add_book():
     name = input('Enter the new book name: ')
     author = input('Enter the new book author: ')
-
+    
     database.add_book(name, author)
 
 
